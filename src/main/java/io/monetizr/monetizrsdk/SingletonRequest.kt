7427a11def3a1e41d314857ac9511d483529455f
Sergey Bukarev <SergeyBukarev@yandex.ru>
2019-09-24 17:06:06 +0300

    remove unnecessary method

--- src/main/java/io/monetizr/monetizrsdk/SingletonRequest.kt
+++ src/main/java/io/monetizr/monetizrsdk/SingletonRequest.kt
@@ -9,11 +9,6 @@ import com.android.volley.RequestQueue
 import com.android.volley.toolbox.ImageLoader
 import com.android.volley.toolbox.Volley
 
-
-
-/**
- * Creating singleton patter for REST API requests, easier to make calls without class initialization
- */
 class SingletonRequest constructor(context: Context) {
     companion object {
         @Volatile
@@ -26,22 +21,7 @@ class SingletonRequest constructor(context: Context) {
             }
     }
 
-    val imageLoader: ImageLoader by lazy {
-        ImageLoader(requestQueue,
-            object : ImageLoader.ImageCache {
-                private val cache = LruCache<String, Bitmap>(20)
-                override fun getBitmap(url: String): Bitmap {
-                    return cache.get(url)
-                }
-                override fun putBitmap(url: String, bitmap: Bitmap) {
-                    cache.put(url, bitmap)
-                }
-            })
-    }
-
     private val requestQueue: RequestQueue by lazy {
-        // applicationContext is key, it keeps you from leaking the
-        // Activity or BroadcastReceiver if someone passes one in.
         Volley.newRequestQueue(context.applicationContext)
     }
 
