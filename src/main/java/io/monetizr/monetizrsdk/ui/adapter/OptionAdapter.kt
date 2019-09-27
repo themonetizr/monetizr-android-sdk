30b0deea5388cd262e2a8698e95138684a13d6fe
Sergey Bukarev <SergeyBukarev@yandex.ru>
2019-09-27 19:50:04 +0300

    varians output in dialog

--- src/main/java/io/monetizr/monetizrsdk/ui/adapter/OptionAdapter.kt
+++ src/main/java/io/monetizr/monetizrsdk/ui/adapter/OptionAdapter.kt
@@ -6,11 +6,12 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import io.monetizr.monetizrsdk.R
+import io.monetizr.monetizrsdk.dto.HierarchyVariant
 import io.monetizr.monetizrsdk.dto.Option
 import io.monetizr.monetizrsdk.dto.Variant
 
-class OptionAdapter(private val items: ArrayList<String>) : RecyclerView.Adapter<OptionAdapter.ViewHolder>() {
-    var selected: Variant? = null
+class OptionAdapter(private val items: List<HierarchyVariant>, val itemTap: (HierarchyVariant) -> Any) : RecyclerView.Adapter<OptionAdapter.ViewHolder>() {
+    var selected: HierarchyVariant? = null
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val context = parent.context
@@ -21,15 +22,9 @@ class OptionAdapter(private val items: ArrayList<String>) : RecyclerView.Adapter
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = items[position]
-        holder.nameView.text = item.title
-        holder.priceView.text = item.priceV2.formatString()
+        holder.nameView.text = item.id
+        holder.priceView.text = item.price.formatString()
         holder.current = item
-
-        if (selected == item) {
-            holder.checkView.visibility = View.VISIBLE
-        } else {
-            holder.checkView.visibility = View.GONE
-        }
     }
 
     override fun getItemCount(): Int {
@@ -39,13 +34,14 @@ class OptionAdapter(private val items: ArrayList<String>) : RecyclerView.Adapter
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         var nameView: TextView = itemView.findViewById(R.id.nameView)
         var priceView: TextView = itemView.findViewById(R.id.priceView)
-        var checkView: View = itemView.findViewById(R.id.checkView)
-        var current: Variant? = null
+        var current: HierarchyVariant? = null
 
         init {
             itemView.setOnClickListener {
                 selected = current
-                notifyDataSetChanged()
+                if (selected != null) {
+                    itemTap(selected!!)
+                }
             }
         }
     }
--- src/main/java/io/monetizr/monetizrsdk/ui/dialog/OptionsDialog.kt
+++ src/main/java/io/monetizr/monetizrsdk/ui/dialog/OptionsDialog.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import io.monetizr.monetizrsdk.R
+import io.monetizr.monetizrsdk.dto.HierarchyVariant
 import io.monetizr.monetizrsdk.dto.Product
 import io.monetizr.monetizrsdk.misc.Parameters
 import io.monetizr.monetizrsdk.ui.adapter.OptionAdapter
@@ -27,7 +28,7 @@ class OptionsDialog : DialogFragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setStyle(STYLE_NO_TITLE, R.style.DialogNoShadow)
+        setStyle(STYLE_NO_TITLE, R.style.DialogShadow)
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -38,8 +39,25 @@ class OptionsDialog : DialogFragment() {
         super.onViewCreated(view, savedInstanceState)
         val json = arguments!!.getString(Parameters.PRODUCT_JSON)
         val product = Product(JSONObject(json))
+        val hierarchyList = product.variantHierarchy.toList()
 
-        val adapter = OptionAdapter(product.variants)
+        backView.setOnClickListener { initAdapter(hierarchyList) }
+        initAdapter(hierarchyList)
+    }
+
+    private fun initAdapter(hierarchyList: List<HierarchyVariant>) {
+        if (hierarchyList.isEmpty() == false) {
+            createAdapter(hierarchyList, hierarchyList[0].title)
+        }
+    }
+
+    private fun createAdapter(variants: List<HierarchyVariant>, title: String) {
+        titleView.text = title
+        val adapter = OptionAdapter(variants) {
+            if (it.descendants.isEmpty() == false) {
+                initAdapter(it.descendants.toList())
+            }
+        }
         val layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
         listView.setHasFixedSize(true)
         listView.layoutManager = layoutManager
