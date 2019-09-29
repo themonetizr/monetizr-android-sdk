b1f3eca99fe1272336d8c308791656df6cdd6f23
Sergey Bukarev <SergeyBukarev@yandex.ru>
2019-09-29 17:50:10 +0300

    fix configurations bug

--- src/main/java/io/monetizr/monetizrsdk/ui/activity/ProductActivity.kt
+++ src/main/java/io/monetizr/monetizrsdk/ui/activity/ProductActivity.kt
@@ -2,6 +2,7 @@ package io.monetizr.monetizrsdk.ui.activity
 
 import android.content.Context
 import android.content.Intent
+import android.content.res.Configuration
 import android.net.Uri
 import android.os.Bundle
 import android.util.Log
@@ -82,6 +83,7 @@ class ProductActivity : AppCompatActivity(), ShippingRateDialogListener, Options
         userMadeInteraction = true
     }
 
+
     override fun onStop() {
         super.onStop()
         val productTag = intent!!.getStringExtra(Parameters.PRODUCT_TAG)
@@ -103,6 +105,11 @@ class ProductActivity : AppCompatActivity(), ShippingRateDialogListener, Options
         if (hasFocus) {
             hideStatusBar()
         }
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration?) {
+        super.onConfigurationChanged(newConfig)
+        hideStatusBar()
     }
 
     override fun onSaveInstanceState(outState: Bundle?) {
@@ -242,7 +249,7 @@ class ProductActivity : AppCompatActivity(), ShippingRateDialogListener, Options
     //region ui
 
     private fun hideStatusBar() {
-        rootView.systemUiVisibility =
+        window.decorView.systemUiVisibility =
             View.SYSTEM_UI_FLAG_LOW_PROFILE or
                     View.SYSTEM_UI_FLAG_FULLSCREEN or
                     View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
