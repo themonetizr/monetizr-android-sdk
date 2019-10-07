7176728ab37d04633e4e9b2938bd31dba9d2170b
Sergey Bukarev <SergeyBukarev@yandex.ru>
2019-10-07 12:57:12 +0300

    hide status bar on image view activity

--- src/main/java/io/monetizr/monetizrsdk/ui/activity/ProductViewActivity.kt
+++ src/main/java/io/monetizr/monetizrsdk/ui/activity/ProductViewActivity.kt
@@ -2,7 +2,9 @@ package io.monetizr.monetizrsdk.ui.activity
 
 import android.content.Context
 import android.content.Intent
+import android.content.res.Configuration
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import io.monetizr.monetizrsdk.R
@@ -20,6 +22,18 @@ class ProductViewActivity : AppCompatActivity() {
         val images = intent.getStringArrayListExtra(Parameters.PRODUCT_IMAGES)
         initImageAdapter(images)
         closeButtonView.setOnClickListener { finish() }
+
+        hideStatusBar()
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration?) {
+        super.onConfigurationChanged(newConfig)
+        hideStatusBar()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        hideStatusBar()
     }
 
     private fun initImageAdapter(photos: ArrayList<String>) {
@@ -33,9 +47,19 @@ class ProductViewActivity : AppCompatActivity() {
         productImagesView.adapter = imageGalleryAdapter
     }
 
+    private fun hideStatusBar() {
+        window.decorView.systemUiVisibility =
+            View.SYSTEM_UI_FLAG_LOW_PROFILE or
+                    View.SYSTEM_UI_FLAG_FULLSCREEN or
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                    View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
+                    View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+                    View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+    }
+
     companion object {
         fun start(context: Context, images: ArrayList<String>) {
-            val starter = Intent(context, ProductActivity::class.java)
+            val starter = Intent(context, ProductViewActivity::class.java)
             starter.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK
             starter.putExtra(Parameters.PRODUCT_IMAGES, images)
             context.startActivity(starter)
